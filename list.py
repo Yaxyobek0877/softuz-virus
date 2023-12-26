41add72dcb11c5da5e0f63916acaa236e65fd9fb
Yaxyobek0877 <106481261+Yaxyobek0877@users.noreply.github.com>
2023-12-26 09:13:42 +0500

    Update list.py

--- list.py
+++ list.py
@@ -1,1 +1,1 @@
-ls=[{}]
+ls=[{"hello.exe":["https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_0.txt","https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_11.txt"]}]
